2816f0157af94c9558092d379c54fe44a919b5b2
Daniel Haas <dhaas@b12.io>
2018-07-04 03:57:06 +0000

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/b12io/room_with_a_view',
-    version='0.1.1',
+    version='0.1.2',
     zip_safe=False,
 )
