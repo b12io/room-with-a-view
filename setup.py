c92c1f62f3444465ec68d236e16b8185e73aa99d
Lydia Gu <zhenya.gu@gmail.com>
2018-07-17 21:06:58 +0000

    Bump version: 0.1.3 → 0.1.4

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/b12io/room_with_a_view',
-    version='0.1.3',
+    version='0.1.4',
     zip_safe=False,
 )
